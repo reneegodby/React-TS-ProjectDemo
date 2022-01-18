c84eb04a1d528fb053bbe035d9ecaa890632544e
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-01-17 23:28:03 -0500

    finished module 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import './components/Clock'
+import Clock from './components/Clock';
+
+let testProp: string = "Am I getting passed to the Clock component?"
+let optionalProp: string = "You sure are!";
+//Here we are creating two variables (testProp, optionalProp) that both have an assigned data type of string. Let's now go ahead and pass them into the call of our Clock component in App.tsx 
 
 const App: React.FunctionComponent = () => {    //Our arrow function expression is given the name of 'App'. By including ':' after 'App', we denote that what follows the colon is the type that we are assigning to 'App'. Here, we are digging into our named import of 'React', and assigning our 'App' the type of 'FunctionComponent' (this is digging into our node modules, and more specifically, the @types packages that are installed).
   return (
     <div className="App">
       <div className="verticalCenter">
-
+      <Clock testProp ={testProp} optionalProp={optionalProp}/>
       </div>
     </div>
   );
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,8 +6,13 @@ type ClockState = {
   time: Date;
 };
 
-class Clock extends React.Component<ClockState> {
-  constructor(props) {
+type AcceptedProps ={
+  testProp: string,
+  optionalProp?: string
+}
+
+class Clock extends React.Component<AcceptedProps, ClockState> {
+  constructor(props: AcceptedProps) {
     super(props)
         this.state={
             time: new Date()
@@ -20,9 +25,9 @@ class Clock extends React.Component<ClockState> {
     });
   }
 
-  componentWillMount() {
-    this.tick();
-  }
+//   componentWillMount() {
+//     this.tick();
+//   }
 
   componentDidMount() {
     setInterval(() => this.tick(), 1000);
@@ -32,6 +37,8 @@ class Clock extends React.Component<ClockState> {
     return (
       <div>
         <h1>{this.state.time.toLocaleTimeString()}</h1>
+        {/* <p>{this.props.testProp}</p>
+        <p>{this.props.optionalProp}</p> */}
       </div>
     );
   }
